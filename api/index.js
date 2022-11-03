0bc2f45d415f18aacd1f38e6dfce508d866efd39
Sean <seanhan18@hotmail.com>
2022-11-03 18:59:44 -0500

    initial db work

---
+++ api/index.js
@@ -0,0 +1,9 @@
+
+const express = require('express');
+
+const apiRouter = express.Router();
+
+const productsRouter = require('./productsRouter');
+apiRouter.use('/products', productsRouter);
+
+module.exports = apiRouter;
