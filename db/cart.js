d35edc6e368b9d4466d6f419cff3aaee1a394b6f
Sean <seanhan18@hotmail.com>
2022-11-10 20:54:25 -0600

    Early API work, additional work on cart.js

--- db/cart.js
+++ db/cart.js
@@ -1,8 +1,61 @@
 const { client } = require('./');
 
 // async function getCart(){
-
+//     try {
+//         const { rows: cartId } = client.query(`
+//             SELECT id
+//             FROM carts;
+//         `);
+//         const carts = await Promise.all(cartId.map(
+//             cart => get
+//         ))
+//     }catch(error){
+//         throw error
+//     }
 // }
 
+async function createCart(userId, productIds ) {
+    try {
+      const { rows: [cart] } = await client.query(`
+          INSERT INTO carts ("userId", "productIds") 
+            VALUES($1, $2) 
+            RETURNING *;
+          `, [userId, productIds,]);
+  
+      return cart;
+    } catch (error) {
+      throw error
+    }
+  }
 
-module.exports = { }
+async function getCartById(userId) {
+    try {
+      const { rows: [cart] } = await client.query(`
+        SELECT *
+        FROM carts
+        WHERE "userId"=$1;
+      `, [userId]);
+  
+      if (!cart) {
+        throw {
+          error: "error",
+          name: "CartNotFound",
+          message: "Cart was not found"
+        };
+      }
+  
+      return cart;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async function updateCart(userId) {
+      try{
+        
+      } catch (error) {
+          throw error;
+      }
+  }
+
+module.exports = { createCart, getCartById }
