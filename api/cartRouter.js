57315c4f5de28cf25f726c5b60832e64a66e558a
Sean <seanhan18@hotmail.com>
2022-11-20 21:01:12 -0600

    progress on cartRouter.js

--- api/cartRouter.js
+++ api/cartRouter.js
@@ -1,9 +1,48 @@
 const express = require('express');
-
 const cartRouter = express.Router();
+const { createCart, getCartByUserId, getCartById } = require('../db/cart');
+const { requireAdmin, requireUser } = require("./utils");
 
-cartRouter.get('/', (req, res, next) => {
-  res.send('CART')
+cartRouter.get("/:userId", requireUser, async (req, res, next) => {
+    
+  const { userId } = req.params
+
+  try {
+  
+      const cart = await getCartByUserId(userId)
+  
+
+  
+          res.send(cart)
+
+  } catch (error) {
+      res.send({
+          message: "Requested cart was not found",
+          name: "CartNotFoundError",
+          error
+      })
+  }
+})
+
+cartRouter.post('/', async (req, res, next) => {
+  const { productIds } = req.body;
+  const cart = {};
+
+  try {
+    cart.productIds = productIds;
+    if (!req.user) {
+      cart.userId = null
+    }
+    else {
+      cart.userId = req.user.id;
+    }
+    const newCart = await createCart(cart.userId, cart.productIds);
+
+    res.send(newCart);
+
+  } catch (error) {
+    throw (error)
+  }
 })
 
 module.exports = cartRouter;
--- db/cart.js
+++ db/cart.js
@@ -15,6 +15,9 @@ const { client } = require('./');
 // }
 
 async function createCart(userId, productIds) {
+  if(!userId){
+    userId = null
+  }
   try {
     const { rows: [cart] } = await client.query(`
           INSERT INTO carts ("userId", "productIds") 
@@ -37,6 +40,28 @@ async function getCartById(id) {
         FROM carts
         WHERE id=$1;
       `, [id]);
+
+    if (!cart) {
+      throw {
+        error: "error",
+        name: "CartNotFound",
+        message: "Cart was not found"
+      };
+    }
+
+    return cart;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getCartByUserId(userId) {
+  try {
+    const { rows: [cart] } = await client.query(`
+        SELECT *
+        FROM carts
+        WHERE "userId"=$1;
+      `, [userId]);
 
     if (!cart) {
       throw {
@@ -129,4 +154,4 @@ async function destroyCart(id) {
   }
 }
 
-module.exports = { createCart, getCartById, getAllCarts, destroyCart, addToCart, removeFromCart }
+module.exports = { createCart, getCartById, getCartByUserId, getAllCarts, destroyCart, addToCart, removeFromCart }
